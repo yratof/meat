cea59f306de7c28b96fca4b0615a904326342281
Andrew Lazarus <team@eyeversuseye.com>
2015-07-16 10:37:17 +0100

    Added an inline block javascript to meat. Should be interesting...

--- library/js/meat.js
+++ library/js/meat.js
@@ -168,3 +168,30 @@ jQuery(document)
             .attr('target', "_blank");
 
     });
+
+
+
+// Inline block sorted out with javascript. Plain vanilla javascript.
+// Technially, this removes unwanted nodes from inside a DOM node
+
+var utils = {};
+
+utils.clean = function(node) {
+	var child, i, len = node.childNodes.length;
+	if (len === 0) { return; }
+	// iterate backwards, as we are removing unwanted nodes
+	for (i = len; i > 0; i -= 1) {
+		child = node.childNodes[i - 1];
+		// comment node? or empty text node
+		if (child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue) )) {
+			node.removeChild(child);
+		} else {
+			if (child.nodeType === 1) {
+				utils.clean(child);
+			}
+		}
+	}
+};
+
+document.documentElement.className='js';
+utils.clean(document.querySelector('.ib'));
